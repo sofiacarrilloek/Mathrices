f88072f3ac29d797de8f7ed01ba3cdcc4555bc5f
adrianmunoz1624 <51468202+adrianmunoz1624@users.noreply.github.com>
2020-06-16 18:36:59 -0500

    examen22

--- Mathrices/src/mathrices/T2_1EXAMEN.java
+++ Mathrices/src/mathrices/T2_1EXAMEN.java
@@ -195,6 +195,9 @@ public class T2_1EXAMEN extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        T2_8EXAMEN c=new T2_8EXAMEN();
+        c.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
--- Mathrices/src/mathrices/T2_8EXAMEN2.java
+++ Mathrices/src/mathrices/T2_8EXAMEN2.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author AAMC
  */
-public class T2_8EXAMEN extends javax.swing.JFrame {
+public class T2_8EXAMEN2 extends javax.swing.JFrame {
 
     /**
-     * Creates new form T2_8EXAMEN
+     * Creates new form T2_8EXAMEN2
      */
-    public T2_8EXAMEN() {
+    public T2_8EXAMEN2() {
         initComponents();
     }
 
@@ -29,45 +29,61 @@ public class T2_8EXAMEN extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel5 = new javax.swing.JLabel();
         rb1 = new javax.swing.JRadioButton();
         rb2 = new javax.swing.JRadioButton();
         rb3 = new javax.swing.JRadioButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        jLabel5.setText("Dada la siguiente matriz:");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 70, -1, 44));
+
         rb1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
-        rb1.setText("   a) 2,1,4,3");
+        rb1.setText("   a)");
         rb1.setFocusable(false);
-        getContentPane().add(rb1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 290, -1, -1));
+        getContentPane().add(rb1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 360, -1, -1));
 
         rb2.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
-        rb2.setText("   b) 2,4,1,3");
+        rb2.setText("   b)");
         rb2.setFocusable(false);
         rb2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 rb2ActionPerformed(evt);
             }
         });
-        getContentPane().add(rb2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 350, -1, -1));
+        getContentPane().add(rb2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 540, -1, -1));
 
         rb3.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
-        rb3.setText("   c) 4,2,1,3");
+        rb3.setText("   c)");
         rb3.setFocusable(false);
-        getContentPane().add(rb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 410, -1, -1));
+        getContentPane().add(rb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 690, -1, -1));
 
-        jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel3.setText("1.-Sustituir los adjuntos en la matriz. ");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 30, -1, 44));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img_2_8/exa_incorrecto2.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 650, 220, 140));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img_2_8/exa_matriz.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 40, 150, 110));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img_2_8/exa_incorrecto.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 490, 220, 140));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img_2_8/exa_correcto.png"))); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 320, 220, 140));
+
+        jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        jLabel6.setText("¿Cuál es la determinante de la siguiente matriz?");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 240, -1, 44));
 
         jButton1.setText("Verificar");
         jButton1.setFocusable(false);
@@ -85,32 +101,7 @@ public class T2_8EXAMEN extends javax.swing.JFrame {
                 jButton3ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 90, 60));
-
-        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        jLabel5.setText("¿Cuál es el orden verdadero para calculara la inversa de una matriz por el método adjunto?");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 240, -1, 44));
-
-        jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel6.setText("4.-Calcular cada uno de los adjuntos de la matriz.");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 150, -1, 44));
-
-        jLabel7.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel7.setText("    2.-Obtener el determinante de la matriz.  ");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 70, -1, 44));
-
-        jLabel8.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        jLabel8.setText("3.- Comprobación.");
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 110, -1, 44));
-
-        jButton2.setText("Siguiente");
-        jButton2.setFocusable(false);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1080, 600, 90, 60));
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 90, 60));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -122,31 +113,23 @@ public class T2_8EXAMEN extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // Verifica si las respuestas son correctas
         if(rb1.isSelected()){
-            JOptionPane.showMessageDialog(null, "INCORRECTO");
+            JOptionPane.showMessageDialog(null, "CORRECTO");
         }
         if(rb2.isSelected()){
-            JOptionPane.showMessageDialog(null, "CORRECTO");
+            JOptionPane.showMessageDialog(null, "INCORRECTO");
         }
         if(rb3.isSelected()){
             JOptionPane.showMessageDialog(null, "INCORRECTO");
         }
-        
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        T2_1EXAMEN c=new T2_1EXAMEN();
+        T2_8EXAMEN c=new T2_8EXAMEN();
         c.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        T2_8EXAMEN2 c=new T2_8EXAMEN2();
-        c.setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,33 +148,33 @@ public class T2_8EXAMEN extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(T2_8EXAMEN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(T2_8EXAMEN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(T2_8EXAMEN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(T2_8EXAMEN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(T2_8EXAMEN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(T2_8EXAMEN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(T2_8EXAMEN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(T2_8EXAMEN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new T2_8EXAMEN().setVisible(true);
+                new T2_8EXAMEN2().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JRadioButton rb1;
     private javax.swing.JRadioButton rb2;
     private javax.swing.JRadioButton rb3;
